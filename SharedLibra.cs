584d49fd36befe6e2ed889449ee176e7dd7f420f
Antonymos777 <118302598+Antonymos777@users.noreply.github.com>
2023-05-24 22:27:40 +0300

    Add files via upload

---
+++ SharedLibra.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibra
+{
+    public class Singleton
+    {
+       private static Singleton instance; //моё приватное статическое поле класса 
+        private string data;//поле, хранит данные экземпляра класса
+
+        public static Singleton Instance //публичное статическое свойство класса 
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton(); //создание нового экземпляра
+                }
+                return instance;
+            }
+        }
+
+        public string GetData()
+        {
+            return data;
+        }
+
+        public void SetData(string newData)
+        {
+            data = newData;
+        }
+    }
+}

dc0e6fdd13a980a71f7e6821acdedcd29570d0da
Antonymos777 <118302598+Antonymos777@users.noreply.github.com>
2023-05-25 15:43:30 +0300

    MySingleton

---
+++ Sig/Rud/Rud.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SharedLibra;
+
+namespace Rud
+{
+    class Rud
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.Instance;
+
+            // Чтение информации из файла
+            string filePath = "C:\\Users\\Anton\\Source\\Repos\\Sig\\Wruter\\bin\\Debug\\example.txt";
+            string content = string.Empty; //пустая строка
+            if (File.Exists(filePath)) //проверка на существование
+            {
+                content = File.ReadAllText(filePath);
+            }
+
+            singleton.SetData(content); //добавляю
+
+            Console.WriteLine("Прочитанная информация из файла: " + singleton.GetData());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sig/Wruter/Wruter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SharedLibra;
+
+namespace Wruter
+{
+    class Wruter
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.Instance; //возвращаю единственный экземпляр класса
+            Console.WriteLine("Введите информацию для записи в файл:");
+            string input = Console.ReadLine();  //запись информации в файл
+            singleton.SetData(input); 
+
+            // создание файла
+            string filePath = "example.txt";
+            using (StreamWriter writer = new StreamWriter(filePath)) //запись символьных данных в файл
+            {
+                writer.WriteLine(input);
+            }
+
+            Console.WriteLine("Информация успешно записана в файл.");
+            Console.ReadLine();
+        }
+
+    }
+}
